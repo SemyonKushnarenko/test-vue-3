31183f41d5bde5bdeea5d127a92d76b279492249
Pavel Khatkevich <pavel.khatkevich2000@gmail.com>
2024-03-27 16:02:06 +0300

    fix

---
+++ src/components/Heroes/Hero.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  hero: Object,
+})
+</script>
+
+<template>
+    <div>
+        <img :src="hero.image" :alt="hero.name" />
+        <p>{{ hero.name }}</p>
+    </div>
+</template>
---
+++ src/components/Search/Search.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+    title: String,
+})
+
+const searchString = ref('')
+</script>
+
+<template>
+    <label :for="title">{{ title }}</label>
+    <input :id="title" type="text" v-model="searchString" @input="$emit('changeSearch', searchString)">
+</template>
+
+<style scoped>
+</style>
